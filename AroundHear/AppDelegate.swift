f47c8a3e4837f0d6a812a6824e12f9b35c0130a3
Guillermo <guille@Guillermos-iMac.local>
2019-03-19 17:18:46 -0700

    Firestore SDK installation

--- AroundHear/AppDelegate.swift
+++ AroundHear/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import Firestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
+        
+        db.collection("cities").document("LA").setData([
+            "name": "Los Angeles",
+            "state": "CA",
+            "country": "USA"
+        ]) { (error: Error?) in
+            if let error = error{
+                print("\(error.localizedDescription)")
+            }else{
+                print("Success")
+            }
+        }
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AroundHear' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AroundHear
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firestore', :podspec => 'https://storage.googleapis.com/firebase-preview-drop/ios/firestore/0.7.0/Firestore.podspec.json'
+
+  target 'AroundHearTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AroundHearUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
